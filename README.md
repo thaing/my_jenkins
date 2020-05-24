b1dfa7135ba28fef923041954eacf9fb03677af0
Thai Nguyen <thai.q.nguyen@gmail.com>
2020-05-23 17:07:16 -0700

    Generate new cookbook my_jenkins

---
+++ README.md
@@ -0,0 +1,4 @@
+# my_jenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: my_jenkins
+# Recipe:: default
+#
+# Copyright:: 2020, Thai Nguyen, All Rights Reserved.
